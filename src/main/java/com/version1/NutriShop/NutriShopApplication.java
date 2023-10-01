61bf2cbc76371c396705721367b0c15c54db6766
Fpalma11 <fabriciopalma17@gmail.com>
2023-09-30 23:57:29 -0600

    Primer Commit proyecto NutriShop

---
+++ src/main/java/com/version1/NutriShop/NutriShopApplication.java
@@ -0,0 +1,13 @@
+package com.version1.NutriShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/version1/NutriShop/NutriShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.version1.NutriShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
